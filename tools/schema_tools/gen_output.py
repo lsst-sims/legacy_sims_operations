a6f4a91364b9b057542b889b98eb8d9c004e2fcb
Lynne Jones <ljones.uw@gmail.com>
2014-11-19 17:31:55 -0800

    Removed ETC and slalib (and thus perry sky brightness) from gen_output.py

--- tools/schema_tools/gen_output.py
+++ tools/schema_tools/gen_output.py
@@ -4,12 +4,8 @@ import sys, re, time, socket
 import math
 import MySQLdb as mysqldb
 import os
-from ETC import ETC
 from socket import gethostname
-try:
-	import slalib
-except:
-    import pysla as slalib
+
 
 def connect_db(hostname='localhost', username='www', passwdname='zxcvbnm', dbname='OpsimDB'):   
     # connect to lsst_pointings (or other) mysql db, using account that has 'alter table' privileges
@@ -46,8 +42,8 @@ def insertDbData(database, sql):
 	del (cursor)
 
 def check_columns_if_they_exist(hname, database, sessionID):
-	columns = ("obsHistID", "sessionID", "fieldID", "fieldRA", "fieldDec", "filter", "expDate", "expMJD", "night", "visitTime", "visitExpTime", "finRank", "finSeeing", "transparency", "airmass", "vSkyBright", "filtSkyBright", "rotSkyPos", "lst", "altitude", "azimuth", "dist2Moon", "solarElong", "moonRA", "moonDec", "moonAlt", "moonAZ", "moonPhase", "sunAlt", "sunAz", "phaseAngle", "rScatter", "mieScatter", "moonIllum", "moonBright", "darkBright", "rawSeeing", "wind", "humidity", "slewDist", "slewTime", "fivesigma", "perry_skybrightness", "fivesigma_ps", "skybrightness_modified", "fivesigma_modified");
-	simname = "output_%s_%d" % (hname, sessionID);    
+	columns = ("obsHistID", "sessionID", "fieldID", "fieldRA", "fieldDec", "filter", "expDate", "expMJD", "night", "visitTime", "visitExpTime", "finRank", "finSeeing", "transparency", "airmass", "vSkyBright", "filtSkyBright", "rotSkyPos", "lst", "altitude", "azimuth", "dist2Moon", "solarElong", "moonRA", "moonDec", "moonAlt", "moonAZ", "moonPhase", "sunAlt", "sunAz", "phaseAngle", "rScatter", "mieScatter", "moonIllum", "moonBright", "darkBright", "rawSeeing", "wind", "humidity", "slewDist", "slewTime", "fiveSigmaDepth");
+	simname = "summary_%s_%d" % (hname, sessionID);
 	sqlquery = "describe %s.%s" % (database, simname);
 	columnsexistbool = True;
 
@@ -59,7 +55,7 @@ def check_columns_if_they_exist(hname, database, sessionID):
 	for result in ret:
 		if (result[0] in columnsexist.keys()): # is this index one of the ones we're looking for?
 			columnsexist[result[0]] = True;
-	
+
 	for ind in columns:
 		if (columnsexist[ind] == False):
 			columnsexistbool = False;
@@ -69,7 +65,7 @@ def create_output_table(hname, database, sessionID):
 	# print 'Creating/Recreating Output Table ...'
 	sql = 'use %s' % (database)
 	ret = getDbData(database, sql)
-	sql = 'create table output_%s_%d (obsHistID int(10) unsigned not null, sessionID int(10) unsigned not null, propID int(10), fieldID int(10) unsigned not null, fieldRA double, fieldDec double, filter varchar(8), expDate int(10) unsigned, expMJD double, night int(10) unsigned, visitTime double, visitExpTime double, finRank double, finSeeing double, transparency double, airmass double, vSkyBright double, filtSkyBright double, rotSkyPos double, lst double, altitude double, azimuth double, dist2Moon double, solarElong double, moonRA double, moonDec double, moonAlt double, moonAZ double, moonPhase double, sunAlt double, sunAz double, phaseAngle double, rScatter double, mieScatter double, moonIllum double, moonBright double, darkBright double, rawSeeing double, wind double, humidity double, slewDist double, slewTime double, fivesigma double, perry_skybrightness double, fivesigma_ps double, skybrightness_modified double, fivesigma_modified double);' % (hname, sessionID)
+	sql = 'create table summary_%s_%d (obsHistID int(10) unsigned not null, sessionID int(10) unsigned not null, propID int(10), fieldID int(10) unsigned not null, fieldRA double, fieldDec double, filter varchar(8), expDate int(10) unsigned, expMJD double, night int(10) unsigned, visitTime double, visitExpTime double, finRank double, finSeeing double, transparency double, airmass double, vSkyBright double, filtSkyBright double, rotSkyPos double, lst double, altitude double, azimuth double, dist2Moon double, solarElong double, moonRA double, moonDec double, moonAlt double, moonAZ double, moonPhase double, sunAlt double, sunAz double, phaseAngle double, rScatter double, mieScatter double, moonIllum double, moonBright double, darkBright double, rawSeeing double, wind double, humidity double, slewDist double, slewTime double, fiveSigmaDepth double);' % (hname, sessionID)
 	ret = getDbData(database, sql)
 	sql = 'select obsHistID, Session_sessionID as sessionID, Field_fieldID as fieldID, filter, expDate, expMJD, night, visitTime, visitExpTime, finRank, finSeeing, transparency, airmass, vSkyBright, filtSkyBright, rotSkyPos, lst, alt as altitude, az as azimuth, dist2Moon, solarElong, moonRA, moonDec, moonAlt, moonAZ, moonPhase, sunAlt, sunAz, phaseAngle, rScatter, mieScatter, moonIllum, moonBright, darkBright, rawSeeing, wind, humidity from ObsHistory where Session_sessionID = %d;' % (sessionID)
 	ret = getDbData(database, sql)
@@ -92,7 +88,7 @@ def create_output_table(hname, database, sessionID):
 		prp = getDbData(database, sql)
 	
 		for i in range(len(prp)):
-			etc = ETC();
+			#etc = ETC();
 			filter = ret[k][3];
 			bandindex = -1;
 			if filter == 'u':
@@ -115,14 +111,11 @@ def create_output_table(hname, database, sessionID):
 			moonDec_RAD = float(ret[k][22]);
 			lst_RAD = float(ret[k][16]);
 			moonha_RAD = lst_RAD - moonRA_RAD;
-			(moonAz_RAD,d1,d2,moonAlt_RAD,d4,d5,d6,d7,d8) = slalib.sla_altaz(moonha_RAD, moonDec_RAD, (ctioLat*DEG2RAD));		
 			malt_RAD = float(ret[k][23]);
 			mphase = float(ret[k][25]);
 			saz = float(ret[k][27]);
 			salt = float(ret[k][26]);
 			mphase = math.acos((mphase/50)-1)*180/math.pi;
-			skyB = etc.getSkyBrightnessAB(bandindex, MJD, extC, (alt_RAD * RAD2DEG), (az_RAD * RAD2DEG), (malt_RAD * RAD2DEG) , (moonAz_RAD * RAD2DEG), mphase, (saz * RAD2DEG), (salt * RAD2DEG));
-		
 			# 5 sigma calculations
 			seeing = float(ret[k][10]);
 			airmass = float(ret[k][12]);
@@ -151,17 +144,16 @@ def create_output_table(hname, database, sessionID):
 			elif filter == 'y1':
 				Cm = 23.74;
 				kAtm = 0.11;
-		
+
 			tauCloud = 0.;
 			expTime = (expTime - 4.00);
 			m5_1 = Cm + 0.50*(filtsky-21) + 2.5*math.log10(0.7/seeing) +1.25*math.log10(expTime/30) - kAtm*(airmass-1) + 1.1*tauCloud; # Using Kem's SkyBrightness
-			m5_2 = Cm + 0.50*(skyB-21) + 2.5*math.log10(0.7/seeing) +1.25*math.log10(expTime/30) - kAtm*(airmass-1) + 1.1*tauCloud; # Using Perry's SkyBrightness
 			# Feature 120: Add 2 columns: skybrightness (ETC for dark and filtsky for z & y filters for twilight) and 5 sigma from this column
 			if (salt*RAD2DEG) > -18.0 and (filter == 'y' or filter == 'z'):
 				# This is if it is twilight that is that sunalt > -18.0 and filter is either y or z
-				sql = 'insert into output_%s_%d (obsHistID, sessionID, propID, fieldID, fieldRA, fieldDec, filter, expDate, expMJD, night, visitTime, visitExpTime, finRank, finSeeing, transparency, airmass, vSkyBright, filtSkyBright, rotSkyPos, lst, altitude, azimuth, dist2Moon, solarElong, moonRA, moonDec, moonAlt, moonAZ, moonPhase, sunAlt, sunAz, phaseAngle, rScatter, mieScatter, moonIllum, moonBright, darkBright, rawSeeing, wind, humidity, slewDist, slewTime, fivesigma, perry_skybrightness, fivesigma_ps, skybrightness_modified, fivesigma_modified) values (%d, %d, %d, %d, %f, %f, "%s", %d, %f, %d, %f, %f, %f, %f, %f, %f, %f, %f, %f, %f, %f, %f, %f, %f, %f, %f, %f, %f, %f, %f, %f, %f, %f, %f, %f, %f, %f, %f, %f, %f, %f, %f, %f, %f, %f, %f, %f)' % (hname, sessionID, ret[k][0], ret[k][1], prp[i][0], ret[k][2], math.radians(fld[0][0]), math.radians(fld[0][1]), ret[k][3], ret[k][4], ret[k][5], ret[k][6], ret[k][7], ret[k][8], ret[k][9], ret[k][10], ret[k][11], ret[k][12], ret[k][13], ret[k][14], ret[k][15], ret[k][16], ret[k][17], ret[k][18], ret[k][19], ret[k][20], ret[k][21], ret[k][22], ret[k][23], ret[k][24], ret[k][25], ret[k][26], ret[k][27], ret[k][28], ret[k][29], ret[k][30], ret[k][31], ret[k][32], ret[k][33], ret[k][34], ret[k][35], ret[k][36], slw[0][1], slw[0][0], m5_1, skyB, m5_2, filtsky, m5_1);
+				sql = 'insert into summary_%s_%d (obsHistID, sessionID, propID, fieldID, fieldRA, fieldDec, filter, expDate, expMJD, night, visitTime, visitExpTime, finRank, finSeeing, transparency, airmass, vSkyBright, filtSkyBright, rotSkyPos, lst, altitude, azimuth, dist2Moon, solarElong, moonRA, moonDec, moonAlt, moonAZ, moonPhase, sunAlt, sunAz, phaseAngle, rScatter, mieScatter, moonIllum, moonBright, darkBright, rawSeeing, wind, humidity, slewDist, slewTime, fivesigma) values (%d, %d, %d, %d, %f, %f, "%s", %d, %f, %d, %f, %f, %f, %f, %f, %f, %f, %f, %f, %f, %f, %f, %f, %f, %f, %f, %f, %f, %f, %f, %f, %f, %f, %f, %f, %f, %f, %f, %f, %f, %f, %f, %f)' % (hname, sessionID, ret[k][0], ret[k][1], prp[i][0], ret[k][2], math.radians(fld[0][0]), math.radians(fld[0][1]), ret[k][3], ret[k][4], ret[k][5], ret[k][6], ret[k][7], ret[k][8], ret[k][9], ret[k][10], ret[k][11], ret[k][12], ret[k][13], ret[k][14], ret[k][15], ret[k][16], ret[k][17], ret[k][18], ret[k][19], ret[k][20], ret[k][21], ret[k][22], ret[k][23], ret[k][24], ret[k][25], ret[k][26], ret[k][27], ret[k][28], ret[k][29], ret[k][30], ret[k][31], ret[k][32], ret[k][33], ret[k][34], ret[k][35], ret[k][36], slw[0][1], slw[0][0], m5_1);
 			else :
-				sql = 'insert into output_%s_%d (obsHistID, sessionID, propID, fieldID, fieldRA, fieldDec, filter, expDate, expMJD, night, visitTime, visitExpTime, finRank, finSeeing, transparency, airmass, vSkyBright, filtSkyBright, rotSkyPos, lst, altitude, azimuth, dist2Moon, solarElong, moonRA, moonDec, moonAlt, moonAZ, moonPhase, sunAlt, sunAz, phaseAngle, rScatter, mieScatter, moonIllum, moonBright, darkBright, rawSeeing, wind, humidity, slewDist, slewTime, fivesigma, perry_skybrightness, fivesigma_ps, skybrightness_modified, fivesigma_modified) values (%d, %d, %d, %d, %f, %f, "%s", %d, %f, %d, %f, %f, %f, %f, %f, %f, %f, %f, %f, %f, %f, %f, %f, %f, %f, %f, %f, %f, %f, %f, %f, %f, %f, %f, %f, %f, %f, %f, %f, %f, %f, %f, %f, %f, %f, %f, %f)' % (hname, sessionID, ret[k][0], ret[k][1], prp[i][0], ret[k][2], math.radians(fld[0][0]), math.radians(fld[0][1]), ret[k][3], ret[k][4], ret[k][5], ret[k][6], ret[k][7], ret[k][8], ret[k][9], ret[k][10], ret[k][11], ret[k][12], ret[k][13], ret[k][14], ret[k][15], ret[k][16], ret[k][17], ret[k][18], ret[k][19], ret[k][20], ret[k][21], ret[k][22], ret[k][23], ret[k][24], ret[k][25], ret[k][26], ret[k][27], ret[k][28], ret[k][29], ret[k][30], ret[k][31], ret[k][32], ret[k][33], ret[k][34], ret[k][35], ret[k][36], slw[0][1], slw[0][0], m5_1, skyB, m5_2, skyB, m5_2);
+				sql = 'insert into summary_%s_%d (obsHistID, sessionID, propID, fieldID, fieldRA, fieldDec, filter, expDate, expMJD, night, visitTime, visitExpTime, finRank, finSeeing, transparency, airmass, vSkyBright, filtSkyBright, rotSkyPos, lst, altitude, azimuth, dist2Moon, solarElong, moonRA, moonDec, moonAlt, moonAZ, moonPhase, sunAlt, sunAz, phaseAngle, rScatter, mieScatter, moonIllum, moonBright, darkBright, rawSeeing, wind, humidity, slewDist, slewTime, fivesigma) values (%d, %d, %d, %d, %f, %f, "%s", %d, %f, %d, %f, %f, %f, %f, %f, %f, %f, %f, %f, %f, %f, %f, %f, %f, %f, %f, %f, %f, %f, %f, %f, %f, %f, %f, %f, %f, %f, %f, %f, %f, %f, %f, %f)' % (hname, sessionID, ret[k][0], ret[k][1], prp[i][0], ret[k][2], math.radians(fld[0][0]), math.radians(fld[0][1]), ret[k][3], ret[k][4], ret[k][5], ret[k][6], ret[k][7], ret[k][8], ret[k][9], ret[k][10], ret[k][11], ret[k][12], ret[k][13], ret[k][14], ret[k][15], ret[k][16], ret[k][17], ret[k][18], ret[k][19], ret[k][20], ret[k][21], ret[k][22], ret[k][23], ret[k][24], ret[k][25], ret[k][26], ret[k][27], ret[k][28], ret[k][29], ret[k][30], ret[k][31], ret[k][32], ret[k][33], ret[k][34], ret[k][35], ret[k][36], slw[0][1], slw[0][0], m5_1);
 			#print sql
 			insertDbData(database, sql)
 
