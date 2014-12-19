6dd8ebe1cc047489a962693ba828999f1557a552
Michael Reuter <mareuternh@gmail.com>
2014-12-19 10:31:28 -0500

    Adding registry tests.

--- tests/testConfigHelpers.py
+++ tests/testConfigHelpers.py
@@ -1,18 +1,26 @@
 import unittest
 
-import lsst.sims.operations.config.helpers as helpers
+import lsst.sims.operations.config.helpers as configHelpers
 
 class TestConfigHelpers(unittest.TestCase):
 
     def testListProposals(self):
-        proposals = helpers.listProposals()
+        proposals = configHelpers.listProposals()
         self.assertEquals(len(proposals.keys()), 2)
         self.assertEquals(len(proposals.items()), 2)
 
     def testLoadProposals(self):
-        proposals = helpers.loadProposals("SouthCelestialPole")
+        proposals = configHelpers.loadProposals("SouthCelestialPole")
         self.assertEquals(proposals[0].name, "SouthCelestialPole-18-0824")
         self.assertEquals(proposals[0].eventSequence.NVisits, 3)
+
+    def testStandardProposalRegistry(self):
+        props = configHelpers.standardPropReg
+        self.assertEquals(len(props.items()), 1)
+
+    def testTransientProposalRegistry(self):
+        props = configHelpers.transientPropReg
+        self.assertEquals(len(props.items()), 1)
 
 if __name__ == "__main__":
     unittest.main()
