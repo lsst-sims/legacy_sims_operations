c9526e550f06f466f23a1e8cae1e12b3cb2b0cb0
Michael Reuter <mareuternh@gmail.com>
2014-12-29 17:49:49 -0500

    Reducing DB connections.

--- tools/schema_tools/move_data_output_obshistory.py
+++ tools/schema_tools/move_data_output_obshistory.py
@@ -19,46 +19,38 @@ def connect_db(hostname='localhost', username='www', passwdname='zxcvbnm', dbnam
     db.autocommit(True)
     return cursor
 
-def getDbData(database, sql):
+def getDbData(cursor, sql):
     global sessionID
-    cursor = connect_db(dbname=database)
     # Fetch the data from the DB
     ret = {}
     try:
-        n = cursor.execute (sql)
-	ret = cursor.fetchall ()
+        n = cursor.execute(sql)
+        ret = cursor.fetchall()
     except:
-        sys.stderr.write('Unable to execute SQL query (%s)\n' % (sql) )
-    # Close the connection
-    cursor.close ()
-    del (cursor)
-    return (ret)
+        sys.stderr.write('Unable to execute SQL query (%s)\n' % (sql))
+    return ret
 
-def insertDbData(database, sql):
+def insertDbData(cursor, sql):
 	global sessionID
-	cursor = connect_db(dbname=database)
 	try:
 		cursor.execute(sql)
 	except:
-		sys.stderr.write('Unable to execute SQL query (%s)\n' % (sql) )
-	# Close the connection
-	cursor.close ()
-	del (cursor)
+		sys.stderr.write('Unable to execute SQL query (%s)\n' % (sql))
 
-def copy_data_over(hname, database, sessionID):
+def copy_data_over(hname, database, cursor, sessionID):
 	# print 'Copying data over'
-	sql = 'use %s' % (database)
-        ret = getDbData(database, sql)
-	sql = 'select obsHistID, fiveSigmaDepth, ditheredRA, ditheredDec from summary_%s_%d' % (hname, sessionID)
-	ret = getDbData(database, sql)
+    sql = 'use %s' % (database)
+    ret = getDbData(cursor, sql)
+    sql = 'select obsHistID, fiveSigmaDepth, ditheredRA, ditheredDec from summary_%s_%d' % (hname, sessionID)
+    ret = getDbData(cursor, sql)
 
-	for k in range(len(ret)):
-		obsHistID = ret[k][0]
-		fiveSigmaDepth = ret[k][1]
-		ditheredRA = ret[k][2]
-		ditheredDec = ret[k][3]
-		sql = 'update tObsHistory_%s_%d set fiveSigmaDepth=%f, ditheredRA=%f, ditheredDec=%f where obsHistID=%d' % (hname, sessionID, fiveSigmaDepth, ditheredRA, ditheredDec, obsHistID)
-		insertDbData(database, sql)
+    for k in range(len(ret)):
+	   obsHistID = ret[k][0]
+	   fiveSigmaDepth = ret[k][1]
+	   ditheredRA = ret[k][2]
+	   ditheredDec = ret[k][3]
+	   sql = 'update tObsHistory_%s_%d set fiveSigmaDepth=%f, ditheredRA=%f, ditheredDec=%f where obsHistID=%d' % (hname, sessionID, fiveSigmaDepth, ditheredRA, ditheredDec, obsHistID)
+	   insertDbData(cursor, sql)
 
 if __name__ == "__main__":
     import sys
@@ -68,4 +60,5 @@ if __name__ == "__main__":
     hname = sys.argv[1]
     database = sys.argv[2]
     sessionID = sys.argv[3]
-    copy_data_over(hname, database, int(sessionID));
+    cursor = connect_db(dbname=database)
+    copy_data_over(hname, database, cursor, int(sessionID));
